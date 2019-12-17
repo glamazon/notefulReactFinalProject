2d5139f428659c677e8e0d6f68e7cfc1f1175909
Peggy Sturman <glamazon5@gmail.com>
2019-12-16 21:23:10 -0800

    made changes for errors

--- src/Error.js
+++ src/Error.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Error.css'
 
-export default function Error(props) {
-  if (props.message) {
-    return (
-      <div className="error">{props.message}</div>
-    )
+class Error extends React.Component {
+  state = {
+    hasError: false
   }
-  return <></>
+
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true
+    }
+  }
+
+  render() {
+    if (this.state.hasError === false) {
+      return this.props.children
+    }
+
+    return (
+      <div>
+        <p className="error">An error has occured, please reload the page.</p>
+      </div>
+    );
+
+  }
 }
+
+
+export default Error
